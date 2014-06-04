8e2dfc5c2c29299aae9d042772ecb1d0cc6d5b15
Hackbright Student <info@hackbrightacademy.com>
2014-06-04 16:55:25 -0700

    Functional calculator with indefinite queries

--- calculator.py
+++ calculator.py
@@ -1,28 +1,31 @@
 def main():
-    print ">"
-    input = raw_input('>')
-    tokens = input.split(' ')
     
-    if tokens[0]=='q':
-        quit()
-    elif tokens[0]=='+':
-        print add(int(tokens[1]),int(tokens[2]))
-    elif tokens[0]=='-':
-        print subtract(int(tokens[1]),int(tokens[2]))
-    elif tokens[0]=='*':
-        print multiply(int(tokens[1]),int(tokens[2]))
-    elif tokens[0]=='square':
-        print square(int(tokens[1]))
-    elif tokens[0]=='/':
-        print divide(int(tokens[1]),int(tokens[2]))
-    elif tokens[0]=='cube':
-        print cube(int(tokens[1]))
-    elif tokens[0]=='^':
-        print power(int(tokens[1]),int(tokens[2]))
-    elif tokens[0]=='mod':
-        print mod(int(tokens[1]),int(tokens[2]))
-    else:
-        print "I don't understand"
+    while True:
+        print ">"
+        input = raw_input('>')
+        tokens = input.split(' ')
+
+        if tokens[0]=='q':
+            quit()
+        elif tokens[0]=='+':
+            print add(int(tokens[1]),int(tokens[2]))
+        elif tokens[0]=='-':
+            print subtract(int(tokens[1]),int(tokens[2]))
+        elif tokens[0]=='*':
+            print multiply(int(tokens[1]),int(tokens[2]))
+        elif tokens[0]=='square':
+            print square(int(tokens[1]))
+        elif tokens[0]=='/':
+            print divide(int(tokens[1]),int(tokens[2]))
+        elif tokens[0]=='cube':
+            print cube(int(tokens[1]))
+        elif tokens[0]=='^':
+            print power(int(tokens[1]),int(tokens[2]))
+        elif tokens[0]=='mod':
+            print mod(int(tokens[1]),int(tokens[2]))
+        else:
+            print "I don't understand"
+
 
 
 def add(num1, num2):
