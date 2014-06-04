ed30b1f38426a4939481589c1499417a289ae259
Hackbright Student <info@hackbrightacademy.com>
2014-06-04 16:22:10 -0700

    function take user input and tokenizes it

---
+++ calculator.py
@@ -0,0 +1,40 @@
+def main():
+    print ">"
+    input = raw_input('>')
+    tokens = input.split(' ')
+    print tokens
+    
+#    if tokens[0]=='q':
+#        quit()
+#    elif tokens[0]=='+':
+ #       print add(int(tokens[1]),int(tokens[2])
+
+
+
+
+'''def add(num1, num2):
+    return num1 + num2
+
+def subtract(num1, num2):
+    return num1 - num2
+
+def multiply(num1, num2):
+    return num1 * num2
+
+def divide(num1, num2):
+    return float(num1) / float(num2)
+
+def square(num1):
+    return num1 * num1
+
+def cube(num1):
+    return num1 * num1 * num1
+
+def power(num1, num2):
+    return num1 ^ num2
+
+def mod(num1, num2):'''
+#    return num1 % num2
+
+if __name__=="__main__":
+    main()
